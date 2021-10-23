1bb7bf5cb4a52bd71b97bfa4a196b21c0238a8c9
leonardobarone <leonardobarone26@gmail.com>
2021-10-23 20:22:05 +0200

    stampato anche con vue

--- api/index.php
+++ api/index.php
@@ -3,5 +3,6 @@
 require __DIR__ . '/../materiale/database.php';
 
 header("Content-type: application/json");
+// header('Access-Control-Allow-Origin: *');
 
 echo json_encode($database);
--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const app = new Vue({
         })
         .catch(function (error) {
             // handle error
-            console.log(error);
+            alert("errori");
         })
     }
 }) 
